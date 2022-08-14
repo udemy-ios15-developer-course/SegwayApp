c472f7fba44204cbc209f2981c4c3cae3d3a285a
Chris Hand <cj.hand@gmail.com>
2022-08-13 22:00:59 -0700

    Create an app with a second view controller

---
+++ SegwayApp/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  SegwayApp
+//
+//  Created by Chris Hand on 8/13/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
